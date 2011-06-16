beb5017bbb96f1fb2f1082e09ddf61c856eca55a
Tomer Filiba <tomerf@il.ibm.com>
2011-06-16 10:40:34 +0300

    build.sh improvement

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-python setup.py sdist --formats=gztar register upload
+python setup.py register
+python setup.py sdist --formats=gztar upload
 python setup.py bdist --formats=wininst --plat-name=win32 upload
